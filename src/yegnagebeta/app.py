97ad77a827876a5d622bfa728941899cde2b3bd6
alemselam09 <115496527+alemselam09@users.noreply.github.com>
2026-03-08 18:58:52 +0300

    Refactor YegnaGebetaApp startup method

--- src/yegnagebeta/app.py
+++ src/yegnagebeta/app.py
@@ -1,27 +1,17 @@
 import toga
 from toga.style import Pack
-from toga.style.pack import COLUMN, ROW
 
 class YegnaGebetaApp(toga.App):
+
     def startup(self):
-        # ዋናውን መስኮት ፍጠር
         self.main_window = toga.MainWindow(title=self.formal_name)
 
-        # የዌብ እይታ (Web View) ክፍል ፍጠር
         web_view = toga.WebView(style=Pack(flex=1))
+        web_view.url = "https://test.mychurch.com.et/yegna_gebeta/"
 
-        # የኛ ገበታ ድረ-ገጽን ጫን
-        web_view.url = 'https://test.mychurch.com.et/yegna_gebeta/'
-
-        # የዌብ እይታውን በዋናው መስኮት ላይ አስቀምጥ
         self.main_window.content = web_view
-
-        # መስኮቱን አሳይ
         self.main_window.show()
 
+
 def main():
-    return YegnaGebetaApp('Yegna Gebeta', 'com.alemselam09.yegnagebeta')
-
-if __name__ == '__main__':
-    main()
-
+    return YegnaGebetaApp()
