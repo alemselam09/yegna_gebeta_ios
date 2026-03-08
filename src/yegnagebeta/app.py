067d2e186e2c990e3f91ff6b97a405fcbd7d2fb9
alemselam09 <115496527+alemselam09@users.noreply.github.com>
2026-03-08 17:51:22 +0300

    Add files via upload

---
+++ src/yegnagebeta/app.py
@@ -0,0 +1,26 @@
+import toga
+from toga.style import Pack
+from toga.style.pack import COLUMN, ROW
+
+class YegnaGebetaApp(toga.App):
+    def startup(self):
+        # ዋናውን መስኮት ፍጠር
+        self.main_window = toga.MainWindow(title=self.formal_name)
+
+        # የዌብ እይታ (Web View) ክፍል ፍጠር
+        web_view = toga.WebView(style=Pack(flex=1))
+
+        # የኛ ገበታ ድረ-ገጽን ጫን
+        web_view.url = 'https://test.mychurch.com.et/yegna_gebeta/'
+
+        # የዌብ እይታውን በዋናው መስኮት ላይ አስቀምጥ
+        self.main_window.content = web_view
+
+        # መስኮቱን አሳይ
+        self.main_window.show()
+
+def main():
+    return YegnaGebetaApp('የኛ ገበታ', 'com.alem.yegnagebeta')
+
+if __name__ == '__main__':
+    main()
