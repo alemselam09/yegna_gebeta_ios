d0ab530cb84ac61c648aa9d7fa5348e5af8b833f
alemselam09 <115496527+alemselam09@users.noreply.github.com>
2026-03-08 22:21:41 +0300

    Refactor app.py to use a box layout for web view

--- src/yegnagebeta/app.py
+++ src/yegnagebeta/app.py
@@ -1,15 +1,25 @@
 import toga
 from toga.style import Pack
+from toga.style.pack import COLUMN
+
 
 class YegnaGebetaApp(toga.App):
 
     def startup(self):
+
         self.main_window = toga.MainWindow(title=self.formal_name)
 
-        web_view = toga.WebView(style=Pack(flex=1))
-        web_view.url = "https://test.mychurch.com.et/yegna_gebeta/"
+        webview = toga.WebView(
+            url="https://test.mychurch.com.et/yegna_gebeta/",
+            style=Pack(flex=1)
+        )
 
-        self.main_window.content = web_view
+        box = toga.Box(
+            children=[webview],
+            style=Pack(direction=COLUMN)
+        )
+
+        self.main_window.content = box
         self.main_window.show()
 
 
