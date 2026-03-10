e92ef5d5ba6ed134174081e57b99507041616dd7
alemselam09 <115496527+alemselam09@users.noreply.github.com>
2026-03-10 13:27:54 +0300

    Refactor web view initialization in app.py

--- src/yegnagebeta/app.py
+++ src/yegnagebeta/app.py
@@ -4,13 +4,15 @@ from toga.style import Pack
 class YegnaGebetaApp(toga.App):
     def startup(self):
         self.main_window = toga.MainWindow(title=self.formal_name)
-        web_view = toga.WebView(style=Pack(flex=1))
-        web_view.url = 'https://test.mychurch.com.et/yegna_gebeta/'
+        web_view = toga.WebView(
+            url='https://test.mychurch.com.et/yegna_gebeta/',
+            style=Pack(flex=1)
+        )
         self.main_window.content = web_view
         self.main_window.show()
 
 def main():
-    return YegnaGebetaApp('Yegna Gebeta', 'com.alemselam09.yegnagebeta')
+    return YegnaGebetaApp.main()
 
 if __name__ == '__main__':
     main()
